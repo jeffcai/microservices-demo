a05e270692335580318f36254be7e5fa7329b8f1
Jeff Cai <jeff.caijf@gmail.com>
2019-07-31 15:34:18 +0800

    updated config server, rabbit server and dir configs.

--- account-service/src/main/java/demo/ms/AccountServiceApplication.java
+++ account-service/src/main/java/demo/ms/AccountServiceApplication.java
@@ -3,6 +3,7 @@ package demo.ms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableFeignClients
+@EnableDiscoveryClient
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
--- auth-service/src/main/java/demo/ms/AuthServiceApplication.java
+++ auth-service/src/main/java/demo/ms/AuthServiceApplication.java
@@ -3,9 +3,11 @@ package demo.ms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
